4a97a6d8c409c46b5ff582452d1ecf21c0a80ed3
Jeremy Polley <jeremypolley@gmail.com>
2018-04-05 16:38:30 -0400

    6-56 testing express app with supertest

---
+++ node-tests/server/server.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest')
+const expect = require('expect')
+
+let app = require('./server').app
+
+it('should return hello world response', (done) => {
+  request(app)
+    .get('/')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({error: 'Page not found.'})
+    })
+    .end(done)
+})
+
+it('should return users', (done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({name: 'Jeremy', age: 36})
+    })
+    .end(done)
+})
