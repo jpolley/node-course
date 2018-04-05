a89b99580c59a94be6e49348be8bba1c822f1171
Jeremy Polley <jeremypolley@gmail.com>
2018-04-05 14:41:13 -0400

    6-53 assertions with expect

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,36 @@
+const expect = require('expect')
+
 const utils = require('./utils')
 
 it('should add two numbers', () => {
   let res = utils.add(33, 11)
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`)
-  }
+  expect(res).toBe(44).toBeA('number')
 })
 
 it('should square the number', () => {
   let res = utils.square(3)
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}`)
-  }
+  expect(res).toBe(9).toBeA('number')
 })
+
+it('should set first and last name', () => {
+  let user = {age: 36, location: 'Concord'} 
+  utils.setName(user, 'Jeremy Polley')
+  expect(user).toInclude({
+    firstName: 'Jeremy',
+    lastName: 'Polley'})
+    .toBeA('object')
+})
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(11)
+//   // expect({name: 'Jeremy'}).toEqual({name: 'Jeremy'})
+//   // expect([2,3,4]).toInclude(4)
+//   // expect([2,3,4]).toExclude(1)
+//   // expect({
+//   //   name: 'Jeremy',
+//   //   age: 36,
+//   //   localation: 'Concord'
+//   // }).toInclude({
+//   //   age: 36
+//   // })
+// })
