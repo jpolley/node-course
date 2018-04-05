33aa19c80ca8ac15e033a1984e693a3f87b33b91
Jeremy Polley <jeremypolley@gmail.com>
2018-04-05 16:14:45 -0400

    6-54 testing async code

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -7,10 +7,25 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number')
 })
 
+it('should async add two number', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number')
+    done()
+  })
+})
+
 it('should square the number', () => {
   let res = utils.square(3)
   expect(res).toBe(9).toBeA('number')
 })
+
+it('should async add two number', (done) => {
+  utils.asyncSquare(3, (x) => {
+    expect(x).toBe(9).toBeA('number')
+    done()
+  })
+})
+
 
 it('should set first and last name', () => {
   let user = {age: 36, location: 'Concord'} 
