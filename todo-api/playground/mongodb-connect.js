39c1bdd5bc5a20601df29a7317e2606b120bb309
Jeremy Polley <jeremypolley@gmail.com>
2018-04-07 14:39:13 -0400

    query the db

--- todo-api/playground/mongodb-connect.js
+++ todo-api/playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient
+const {MongoClient, ObjectID} = require('mongodb')
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -18,17 +18,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // })
 
   //Insert new doc into Users (name, age, location)
-  db.collection('Users').insertOne({
-    name: 'Jeremy',
-    age: 36,
-    location: 'Concord, NC'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert todo', err)
-    }
+  // db.collection('Users').insertOne({
+  //   name: 'Jeremy',
+  //   age: 36,
+  //   location: 'Concord, NC'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err)
+  //   }
 
-    console.log(JSON.stringify(result.ops, undefined, 2))
-  })
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2))
+  // })
 
   db.close()
 })
