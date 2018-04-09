3778a4f02d1b1ca8a2a13ba6241bf90c6d641027
Jeremy Polley <jeremypolley@gmail.com>
2018-04-09 06:46:23 -0400

    playing with ruby enumerable

---
+++ playground/enumerable.rb
@@ -0,0 +1,31 @@
+class Order
+  TAX_TABLE = { "CO" => 0.02, "MT" => 0.00, "AZ" => 0.04 }
+
+  attr_reader :email, :total, :state, :status
+
+  def initialize(email, state, total, status = :pending)
+    @email = email
+    @state = state
+    @total = total
+    @status = status
+  end
+
+  def tax
+    total * TAX_TABLE[state]
+  end
+
+  def to_s
+    "#{email} (#{state}): $#{total} - #{status}"
+  end
+end
+
+orders = []
+orders << Order.new("customer1@example.com", "MT", 300)
+orders << Order.new("customer2@example.com", "AZ", 400, :completed)
+orders << Order.new("customer3@example.com", "CO", 200)
+orders << Order.new("customer4@example.com", "CO", 100)
+
+puts "Big orders:"
+big_orders = orders.select { |o| o.total >= 300 }
+puts big_orders
+puts "---"
